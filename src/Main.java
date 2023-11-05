04aff75942f6c10ff986111b7a3b81b5fa5687a9
elizabeth2026 <elizabethl2026@student.cis.edu.hk>
2023-11-05 22:00:46 +0800

    first

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,49 @@ public class Main {
     // 1. parenthesesCheck
     public static boolean parenthesesCheck(String a){
 
-            int half = a.length()/2;
-            if(a.substring(half,half+2).equals("()")){
+        for(int i = 0; i<a.length()-2; i++){
+            char one = a.charAt(i);
+            char two = a.charAt(a.length()-i);
+
+            if(one!=two){
                 return true;
+            }
         }
-            return false;
+        return false;
     }
 
     // 2. reverseInteger
     public static String reverseInteger(int integer){
         int a = 0;
         for (int i = 0; i<integer; i++) {
-            int lastdigit = integer%10;
-            a = (a*10)+lastdigit;
-            if(integer>=1 && integer<9) {
-                a=a+lastdigit;
-            }
-            else
-
+            int lastdigit = 0;
+            if (integer > 9) {
+                lastdigit = integer % 10;
+                a = (a * 10) + lastdigit;
                 integer = (integer - lastdigit) / 10;
 
-
+            } else
+                a = a + lastdigit;
 
 
         }
-
         String b = Integer.toString(a);
         return b;
     }
 
     // 3. encryptThis
-    public static boolean encryptThis(String a) {
-        return true;
+    public static String encryptThis(String a) {
+        String answer = "";
+        for(int i = 0; i<a.length(); i++){
+
+            String letterone = a.substring(0,1);
+            int front = Integer.parseInt(letterone);
+            String middle = a.substring(1,2);
+            String lastletter = a.substring(a.length()-1);
+            String font = Integer.toString(front);
+            answer = font + lastletter + middle;
+        }
+        return answer;
     }
 
     // 4. decipherThis
